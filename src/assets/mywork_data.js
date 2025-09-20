b6ab5c0f904515d6e02b9d800795264cfcb18fba
Heshani Thennakoon <heshanithennakoon118@gmail.com>
2025-09-20 12:28:25 +0530

    Create work section

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,17 @@
+import project1_img from './project1_img.png';
+import project2_img from './project2_img.png'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"web design",
+        w_img:project1_img
+    },
+    {
+        w_no:2,
+        w_name:"face detection system",
+        w_img:project2_img
+    }
+]
+
+export default mywork_data;
